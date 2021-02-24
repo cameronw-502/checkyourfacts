de3d5ca456ce934ee9528a5f5d9a1aff8e832838
Cameron White <camwhite1@outlook.com>
2021-02-24 16:32:37 -0500

    Windows design

---
+++ script.js
@@ -0,0 +1,17 @@
+function def() {
+    document.getElementById("title").innerHTML = "Choose a folder to the left."
+    document.getElementById("content").innerHTML = "";
+    document.getElementById("content-image").src = "img/arrow.jpg";
+}
+
+function health() {
+    document.getElementById("title").innerHTML = "Health";
+    document.getElementById("content").innerHTML = "False information can be detramental to your health. For example, you notice that you have a few flu like symptoms. When you search for the symptoms using a search engine, you are given many different diagnosis that can incorrect.";
+    document.getElementById("content-image").src = "img/health.jpg";
+}
+
+function grades() {
+    document.getElementById("title").innerHTML = "Grades";
+    document.getElementById("content").innerHTML = "This is the health text!!";
+    document.getElementById("content-image").src = "img/health.jpg";
+}
